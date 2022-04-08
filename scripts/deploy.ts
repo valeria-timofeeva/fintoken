16d2903dd441021bd8c5d97f7e159650656d3a01
Valeri <veltimofeeva@gmail.com>
2022-04-08 12:38:44 +0400

    added tasks, deploy for staking

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -7,13 +7,20 @@ async function main() {
   const FintokenFactory = await ethers.getContractFactory("Fintoken");
   const fintokenContract = await FintokenFactory.deploy("Fintoken", "FIN", utils.parseUnits("1000000"));
 
+  const LPFactory = await ethers.getContractFactory("LpTokenMock");
+  const lpContract = await LPFactory.deploy();
+
+  const StakingFactory = await ethers.getContractFactory("Staking");
+  const stakingContract = await StakingFactory.deploy(fintokenContract.address, lpContract.address);
+
   await fintokenContract.deployed();
   console.log("Fintoken deployed, fintoken address:", fintokenContract.address);
 
   const contracts = {
     fintokenAddress: fintokenContract.address,
+    stakingAddress: stakingContract.address,
   };
-  fs.writeFile("./tasks/DeployedFintoken.json", JSON.stringify(contracts), (err) => {
+  fs.writeFile("./tasks/DeployedContracts.json", JSON.stringify(contracts), (err) => {
     if (err) throw err;
   });
 }
